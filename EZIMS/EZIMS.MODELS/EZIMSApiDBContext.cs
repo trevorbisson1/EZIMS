4eaea0bfe7571b6301d090f14563c1a23e431fc2
trevorbisson1 <118816953+trevorbisson1@users.noreply.github.com>
2025-08-07 21:53:35 -0500

    im tired

--- EZIMS/EZIMS.MODELS/EZIMSApiDBContext.cs
+++ EZIMS/EZIMS.MODELS/EZIMSApiDBContext.cs
@@ -16,15 +16,15 @@ namespace EZIMS.MODELS
 
         public required DbSet<Business> Businesses { get; set; }
         public required DbSet<BusinessAction> BusinessActions { get; set; }
-        public required DbSet<BusinessAction_BusinessRole_PivotTable> BusinessAction_BusinessRole_PivotTables { get; set; }
+        //public required DbSet<BusinessAction_BusinessRole_PivotTable> BusinessAction_BusinessRole_PivotTables { get; set; }
         public required DbSet<BusinessRole> BusinessRoles { get; set; }
         public required DbSet<IMS_Users> IMS_Users { get; set; }
         public required DbSet<Item> Items { get; set; }
         public required DbSet<ItemHistory> ItemHistories { get; set; }
-        public required DbSet<Item_ItemType_PivotTable> Item_ItemType_PivotTables { get; set; }
+        //public required DbSet<Item_ItemType_PivotTable> Item_ItemType_PivotTables { get; set; }
         public required DbSet<ItemType> ItemTypes { get; set; }
         public required DbSet<SiteActions> SiteActions { get; set; }
-        public required DbSet<SiteActions_SiteRole_PivotTable> SiteActions_SiteRole_PivotTables { get; set; }
+        //public required DbSet<SiteActions_SiteRole_PivotTable> SiteActions_SiteRole_PivotTables { get; set; }
         public required DbSet<SiteRole> SiteRoles { get; set; }
     }
 }
