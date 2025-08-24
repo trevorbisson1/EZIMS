286d3427b04faaec813806f2dc085be515c98da4
trevorbisson1 <118816953+trevorbisson1@users.noreply.github.com>
2025-08-24 17:04:00 -0500

    idk what this did

--- EZIMS/EZIMS.WEB/src/App.jsx
+++ EZIMS/EZIMS.WEB/src/App.jsx
@@ -3,6 +3,7 @@ function App() {
     const { data: data } = useGetItemsQuery(1);
     return (
         <div>
+        <h3>Hello!</h3>
             {data && <div> <h1> Here: { data}</h1></div> }
         </div>
   )
